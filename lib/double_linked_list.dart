9f9772a3ee3ac1fbb2a5fd1455e23e20821f2a66
Snare295 <vladneum@gmail.com>
2023-06-01 19:09:49 +0300

    Add insert, test for insert of double linked list

--- lib/double_linked_list.dart
+++ lib/double_linked_list.dart
@@ -7,6 +7,7 @@ base class DoubleLinkedList<T> {
       DoubleNode startNode = DoubleNode(data: value);
       firstNode = startNode;
       lastNode = startNode;
+      lengthList = 1;
     }
   }
 
@@ -18,20 +19,59 @@ base class DoubleLinkedList<T> {
     return lengthList;
   }
 
-  void append<T>(T value) {
+  void append(T value) {
     DoubleNode newNode = DoubleNode(data: value);
 
     if (lastNode == null) {
       if (lengthList != 0)
-        throw "DoubleLinkedList length != 0 when lastNode is null";
+        throw FormatException(
+            "DoubleLinkedList length != 0 when lastNode is null");
 
       firstNode = newNode;
       lastNode = newNode;
+      lengthList++;
       return;
     }
 
     lastNode!.next = newNode;
     newNode.previous = lastNode;
     lastNode = newNode;
+    lengthList++;
+  }
+
+  void insert(T value, int index) {
+    if (index < 0) {
+      throw FormatException("DoubleLinkedList.insert retrive a negative index");
+    }
+    if (index > lengthList) {
+      throw FormatException(
+          "DoubleLinkedList.insert index is out of list's length");
+    }
+
+    DoubleNode newNode = DoubleNode(data: value);
+    if (index == lengthList) {
+      this.append(value);
+      return;
+    }
+    if (index == 0) {
+      firstNode!.previous = newNode;
+      newNode.next = firstNode;
+      firstNode = newNode;
+      lengthList++;
+      return;
+    }
+
+    //Insert isn't first or last node
+    DoubleNode before;
+    DoubleNode after = firstNode!;
+    for (var i = 0; i < index; i++) {
+      after = after.next!;
+    }
+    before = after.previous!;
+    before.next = newNode;
+    after.previous = newNode;
+    newNode.next = after;
+    newNode.previous = before;
+    lengthList++;
   }
 }
--- test/double_linked_list_test.dart
+++ test/double_linked_list_test.dart
@@ -14,4 +14,21 @@ void main() {
     expect(l.firstNode!.next!.data, 30);
     expect(l.lastNode!.previous!.data, 20);
   });
+
+  test('Test insert', () {
+    var l = DoubleLinkedList(10);
+    l.append(20);
+    l.append(30);
+    expect(l.length(), 3);
+
+    l.insert(15, l.length());
+    expect(l.lastNode!.data, 15);
+
+    l.insert(-10, 0);
+    expect(l.firstNode!.data, -10);
+    expect(l.length(), 5);
+
+    l.insert(125, 2);
+    expect(l.firstNode!.next!.next!.data, 125);
+  });
 }
